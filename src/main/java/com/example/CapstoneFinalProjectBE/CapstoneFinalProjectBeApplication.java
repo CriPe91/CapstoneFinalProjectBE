d8919b2a6ab05a7275a281d49c8707ec60e9638c
CriPe91 <cristian.petta91@gmail.com>
2025-03-03 14:45:46 +0100

    Initial commit

---
+++ src/main/java/com/example/CapstoneFinalProjectBE/CapstoneFinalProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.CapstoneFinalProjectBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneFinalProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneFinalProjectBeApplication.class, args);
+	}
+
+}
